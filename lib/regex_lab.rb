4e76debffe623adab009c3c6ff1e18ac36715798
Thomas Pope <tpope0928@gmail.com>
2018-07-31 18:36:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if word.match(/\W)
+  if word.match(/\W/[A-Z])
 end
 
 def valid_phone_number?(phone)
